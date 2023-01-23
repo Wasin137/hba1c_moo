65d9581fb738109b0fe9b9ba67eb90c375691846
Wasin137 <wasin.kamp@gmail.com>
2023-01-23 11:40:24 +0700

    Update def

--- main.py
+++ main.py
@@ -31,28 +31,28 @@ left_column, right_column = st.columns(2)
 with left_column:
     inp_month = st.radio('Month:', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
 
-# def make_inp(inp_abo, inp_day, inp_month):
-#     keys = ['ABOGROUP_A', 'ABOGROUP_AB', 'ABOGROUP_B', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_April', 'month_August', 'month_December', 'month_February', 'month_January', 'month_July', 'month_June', 'month_March' ,'month_May', 'month_November', 'month_October', 'month_September']
-#     feature_dict = dict.fromkeys(keys, [])
-#     feature_dict = {k: 1 if k == f"ABOGROUP_{inp_abo}" else 0 for k in feature_dict.keys()}
-#     feature_dict = {k: 1 if k == f"weekday_{inp_day}" else 0 for k in feature_dict.keys()}
-#     feature_dict = {k: 1 if k == f"month_{inp_month}" else 0 for k in feature_dict.keys()}
-#     cols_order = ['ABOGROUP_A', 'ABOGROUP_B', 'ABOGROUP_AB', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_January', 'month_February', 'month_March', 'month_April', 'month_May', 'month_June', 'month_July', 'month_August', 'month_September', 'month_October', 'month_November', 'month_December']
-#     feature_df = pd.DataFrame.from_dict(feature_dict)
-#     feature_df = feature_df[cols_order]
-#     return feature_df
+def make_inp(inp_abo, inp_day, inp_month):
+    keys = ['ABOGROUP_A', 'ABOGROUP_AB', 'ABOGROUP_B', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_April', 'month_August', 'month_December', 'month_February', 'month_January', 'month_July', 'month_June', 'month_March' ,'month_May', 'month_November', 'month_October', 'month_September']
+    feature_dict = dict.fromkeys(keys, 0)
+    for k in feature_dict.keys():
+        if k == f"ABOGROUP_{inp_abo}":
+            feature_dict[k] = 1
+        elif k == f"weekday_{inp_day}":
+            feature_dict[k] = 1
+        elif k == f"month_{inp_month}":
+            feature_dict[k] = 1
+        else:
+            feature_dict[k] = 0
+    cols_order = ['ABOGROUP_A', 'ABOGROUP_B', 'ABOGROUP_AB', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_January', 'month_February', 'month_March', 'month_April', 'month_May', 'month_June', 'month_July', 'month_August', 'month_September', 'month_October', 'month_November', 'month_December']
+    feature_df = pd.DataFrame.from_dict(feature_dict, orient='index', columns=['value'])
+    feature_df = feature_df.transpose().reset_index(drop=True)
+    feature_df = feature_df[cols_order]
+    return feature_df
 
 if st.button('Make Prediction'):
-    # feature_to_predict = make_inp(inp_abo, inp_day, inp_month)
-    # prediction = model.predict(feature_to_predict)
-    # print('Your predicted HbA1c', prediction)
-    # st.write(f'Your predicted Hba1c is: {prediction} mg/dl')
-    keys = ['ABOGROUP_A', 'ABOGROUP_AB', 'ABOGROUP_B', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_April', 'month_August', 'month_December', 'month_February', 'month_January', 'month_July', 'month_June', 'month_March' ,'month_May', 'month_November', 'month_October', 'month_September']
-    feature_dict = dict.fromkeys(keys, [])
-    feature_dict = {k: 1 if k == f"ABOGROUP_{inp_abo}" else 0 for k in feature_dict.keys()}
-    feature_dict = {k: 1 if k == f"weekday_{inp_day}" else 0 for k in feature_dict.keys()}
-    feature_dict = {k: 1 if k == f"month_{inp_month}" else 0 for k in feature_dict.keys()}
-    cols_order = ['ABOGROUP_A', 'ABOGROUP_B', 'ABOGROUP_AB', 'ABOGROUP_O', 'weekday_friday', 'weekday_monday', 'weekday_saturday', 'weekday_sunday', 'weekday_thursday', 'weekday_tuesday', 'weekday_wednesday', 'month_January', 'month_February', 'month_March', 'month_April', 'month_May', 'month_June', 'month_July', 'month_August', 'month_September', 'month_October', 'month_November', 'month_December']
-    feature_df = pd.DataFrame.from_dict(feature_dict)
-    feature_df = feature_df[cols_order]
-    st.write(feature_df)
+    feature_to_predict = make_inp(inp_abo, inp_day, inp_month)
+    st.write(feature_to_predict)
+    prediction = model.predict(feature_to_predict)
+    print('Your predicted HbA1c', prediction)
+    st.write(f'Your predicted Hba1c is: {prediction} mg/dl')
+
